a06c971df05bb95ac4e8ff088c0cac7167725890
Cheka <39255806+Argentix03@users.noreply.github.com>
2020-05-15 20:45:08 +0300

    Add files via upload

--- sscr-server.py
+++ sscr-server.py
@@ -1,7 +1,7 @@
 # SSCR Server - Simple Socket Chat Room Server
-# Version: Alpha 0.5
+# Version: Alpha 0.6
 # This is the server app of the Simple Socket Chat Room which handles all client connections
-# todo: 1. Clean up server side prints to only whats relevant to the eye in realtime and implement a log for later debugging/analysing
+# todo: 1. [DONE] Clean up server side prints to only whats relevant to the eye in realtime and implement a log for later debugging/analysing
 #       2. More bot commands!
 #       3. Permission system for bot commands
 #       4. Generate Token on server lunch for admin to authenticate with when they connect with the client for bots permmissions
@@ -10,13 +10,14 @@
 
 import socket
 import chatbot
+import config
 import sys
 import _thread
 import datetime
+import random
+
 try:
-    room_name = "Default-room 1"
     client_list = {}  # dict of {client(id) -> connections from socket.accept()}
-    name_list = {}  # dict of {id -> name} to keep track of names of users and their connection (which are tied to the id)
     id_counter = 1  # counter that will increment to keep unique ID for every joined user
     server = socket.socket()  # the global server socket variable
     logfile = open('sscr-server.log', 'a') # used for log
@@ -29,14 +30,24 @@ def log(logMessage):
     logfile.write(f"{datetime.datetime.now()}: {logMessage}\n")
     logfile.flush()
 
+# generates a unique token
+def token_gen(length):
+    token = ""
+    for i in range(length):
+        upper_character = chr(random.randint(ord('A'), ord('Z')))
+        lower_character = chr(random.randint(ord('a'), ord('z')))
+        digit = chr(random.randint(48, 57))
+        random_characters = [upper_character, lower_character, digit]
+        token += random.choice(random_characters)
+    return token
+
 # joins a member and return his ID if successful, on fail return -1
 def join(conn, name):
     try:
         global client_list
         global id_counter
-        global name_list
         client_list[id_counter] = conn
-        name_list[id_counter] = name
+        config.name_list[id_counter] = name
         print(f"Added: {name}, ID: {id_counter}")
         log(f"Added: {name}, ID: {id_counter}")
         log(f"Updated client_list: {client_list}")
@@ -52,9 +63,10 @@ def join(conn, name):
 def greet(conn, addr):
     try:
         log(f"New thread: {conn}")
-        conn.send(f"Welcome to {room_name}\nYour address ({addr}) will not be shared with anyone\nPlease Type in your name: ".encode())
+        conn.send(f"Welcome to {config.room_name}\nYour address will not be shared with anyone\nPlease Type in your name: ".encode())
         user_name = conn.recv(1024).decode()
         user_id =  join(conn, user_name)
+        conn.send(f"Your user ID: {user_id}".encode())
         if user_id != -1:
             return user_id
         else:
@@ -75,7 +87,7 @@ def broadcast(message, name):
             client_list[client].send(message.encode())
             log(f"Sent message successfully to {client_list[client]}")
         except Exception as e:
-            log(f"\nFailed to send message to {name_list[client]} ({client})")
+            log(f"\nFailed to send message to {config.name_list[client]} ({client})")
             log(e)
 
 def client_handle(conn, addr):
@@ -86,17 +98,23 @@ def client_handle(conn, addr):
     # find name
     for id, sock in client_list.items():
         if sock == conn:
-            name = name_list[id]
+            name = config.name_list[id]
             userId = id
     broadcast(f"{name} has joined the room", 'Server')
     # command has been executed and this is not a message if this is True
     # for publicly visible commands bot could use broadcast() with a non bot command for the displayed message
     while True:
         message = conn.recv(2048).decode()
-        command = chatbot.evalCommand(message)
+
+        command = chatbot.evalCommand(message, userId)
         if command:
             log(f"command: {command} userID: {userId} name: {name}")
+            if command.startswith("private:"):
+                conn.send("-Command successfully executed-".encode())
+            elif command.startswith("public:"):
+                broadcast(command.split(":")[1], "ChatBot")
         else:
+            name = config.name_list[userId]  # update name before sending message
             broadcast(message, name)
 
 
@@ -107,6 +125,8 @@ def main(port):
         global server
         server.bind(('0.0.0.0', port))
         server.listen(20)
+        config.token = token_gen(12)
+        print(f"Admin token is: {config.token}")
         print("Server is up, waiting for connections...")
 
         while True:
@@ -119,10 +139,10 @@ def main(port):
         print(e)
 
 
-# Simple sanity check before calling main with port number
+Simple sanity check before calling main with port number
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: sscr-Server.py port")
+        print("Usage: sscr-server.py port")
         exit()
     else:
         try:
